3bbb7fdcafb71128885d9e0ab79ee00605648f99
Juan Bertinetti <juanbertinetti@gmail.com>
2019-10-14 13:10:50 -0300

    Hecho que no falle scraping si hay un 404

--- scraping/base_pubmed.py
+++ scraping/base_pubmed.py
@@ -4,13 +4,17 @@ obtiene la URL de las fuentes y las guarda al lado de cada PMID.
 """
 
 import urllib.request
+from urllib.error import HTTPError
 import sys
 
 def fetch_url(url):
     """Descarga y devuelve el contenido de una URL."""
-    with urllib.request.urlopen(url) as f:
-        data = f.read()
-        return data.decode("utf8")
+    try:
+        with urllib.request.urlopen(url) as f:
+            data = f.read()
+            return data.decode("utf8")
+    except HTTPError:
+        return "404"
 
 def get_src_url(pmid):
     """Dado un PMID, devuelve la URL de la fuente."""
