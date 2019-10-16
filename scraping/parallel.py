25b5f5838fe5144c2570eef817bf5eb16c64b890
Juan Bertinetti <juanbertinetti@gmail.com>
2019-10-16 20:32:15 -0300

    Funciones para paralelizar

---
+++ scraping/parallel.py
@@ -0,0 +1,60 @@
+import multiprocessing
+import math
+
+def parallel_map(func, elements):
+    """
+    Aplica de forma paralela la función 'func' en toda la colección 'elements'. Básicamente
+    sería: res = [func(x) for x in elements] pero paralelamente.
+    """
+    cant = len(elements)
+    cpus = multiprocessing.cpu_count()
+    chunksize = int(math.ceil(cant / cpus))
+    outq = multiprocessing.Queue()
+    jobs = []
+    for i in range(cpus):
+        chunk = elements[chunksize * i:chunksize * (i + 1)]
+        thread = multiprocessing.Process(
+            target=self.process_chunk, args=(chunk, i, outq))
+        jobs.append(thread)
+        thread.start()
+
+    res = []
+    for i in range(cpus):
+        res.append(outq.get())
+
+    for j in jobs:
+        j.join()
+
+    res.sort(key=operator.itemgetter(0)) #ordeno por índices de chunks
+    # for t in res:
+        # self.data.extend(t[1])
+
+def process_chunk(chunk, chunk_index, outq):
+    outq.put((chunk_index, mls))
+
+
+def parallel_map_to_file(func, elements, outfile):
+    """
+    Aplica de forma paralela la función 'func' en toda la colección 'elements'. Básicamente
+    sería: res = [func(x) for x in elements] pero paralelamente.
+    """
+    cant = len(elements)
+    cpus = multiprocessing.cpu_count()
+    chunksize = int(math.ceil(cant / cpus))
+    outq = multiprocessing.Queue()
+    jobs = []
+    for i in range(cpus):
+        chunk = elements[chunksize * i:chunksize * (i + 1)]
+        thread = multiprocessing.Process(
+            target=process_filechunk, args=(func, chunk, i, outfile))
+        jobs.append(thread)
+        thread.start()
+    for j in jobs:
+        j.join()
+    # unir archivos
+
+def process_filechunk(func, chunk, chunk_index, outfile):
+    with open("{}_{}".format(outfile, chunk_index), "w") as out:
+        for element in chunk:
+            out.write(func(element) + "\n")
+    
