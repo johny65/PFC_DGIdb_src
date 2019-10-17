1ced9491ad809e323d882e17914855e06fd06f1a
Darién Julián Ramírez <ianshalaga@gmail.com>
2019-10-17 18:45:30 -0300

    -

--- scraping/ncbi.py
+++ scraping/ncbi.py
@@ -1,15 +1,6 @@
 """
 Web scraping para National Center for Biotechnology Information (www.ncbi.nlm.nih.gov).
 """
-import urllib.request
-from urllib.error import URLError
+from html.parser import HTMLParser
+import utils
 
-def scrap(url):
-    try:
-        res = urllib.request.urlopen(url)
-        if res.getcode() == 200:
-            # redirige a otra url que termina en .long
-            # si se reemplaza .long por .full.pdf se obtiene el pdf
-            return res.geturl().replace(".long", ".full.pdf")
-    except URLError as e:
-        return None
