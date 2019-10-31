13da09f985de66af7587fb389381502b949b1ca9
Juan Bertinetti <juanbertinetti@gmail.com>
2019-10-31 18:08:10 -0300

    Scripts de preproceso

--- scraping/varios/csv.py
+++ scraping/varios/csv.py
@@ -1,7 +1,48 @@
-import utils
+import sys
+import _csv
+from _csv import writer as ww
+from _csv import reader as rr
+# import utils
 
-with open("pmids") as f:
-    for l in f:
-        pmid, url = l.split()[0], l.split()[1]
-        server = utils.get_server(url) if url != "No" else "-"
-        print("{},{}".format(pmid, server))
+def pmid_src():
+    """Guarda en un archivo el pmid y su fuente."""
+    with open("pmids") as f:
+        for l in f:
+            pmid, url = l.split()[0], l.split()[1]
+            server = utils.get_server(url) if url != "No" else "-"
+            print("{},{}".format(pmid, server))
+
+
+def getCsvReader(reading_file):
+    return rr(reading_file, delimiter=',', quoting=_csv.QUOTE_ALL)
+
+
+def generate_training(ifg_file, abstracts_file, out_file):
+    """
+    Genera un archivo donde cada fila es:
+    [pmid, gen, droga, interacción, abstract]
+    Ejemplo: 10722,ADRA1A,MEPHENTERMINE,agonist,el abstract
+    """
+    abstracts = {}
+    with open(abstracts_file) as afile:
+        for l in afile:
+            ll = l.split()
+            pmid = ll[0]
+            a = " ".join(ll[1:])
+            abstracts[pmid] = a
+
+    out = open(out_file, "w")
+    writer = ww(out, delimiter='\t')
+
+    with open(ifg_file) as csvfile:
+        reader = getCsvReader(csvfile)
+        for row in reader:
+            out_line = list(row) + [abstracts[row[0]]]
+            writer.writerow(out_line)
+    out.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("ifg_file, abstracts_file, out_file")
+        exit()
+    generate_training(sys.argv[1], sys.argv[2], sys.argv[3])
--- scraping/varios/remove_absent.py
+++ scraping/varios/remove_absent.py
@@ -1,11 +1,32 @@
-pmids = open("pmids")
-clean = open("pmids_clean")
-out = open("outs", "w")
-for l in clean:
-    lorig = pmids.readline()
-    if "cookieAbsent" in l:
-        l = lorig
-    out.write(l)
-out.close()
-pmids.close()
-clean.close()
+import sys
+
+def remove_cookie_absent():
+    pmids = open("pmids")
+    clean = open("pmids_clean")
+    out = open("outs", "w")
+    for l in clean:
+        lorig = pmids.readline()
+        if "cookieAbsent" in l:
+            l = lorig
+        out.write(l)
+    out.close()
+    pmids.close()
+    clean.close()
+
+
+def sin_abstract(infile):
+    pmids = []
+    with open(infile) as f:
+        for l in f:
+            if "N/A" == l.split()[1]:
+                print(l)
+                pmids.append(l.split()[0])
+    print("Total N/A:", len(pmids))
+    print("UPDATE publicaciones SET abstract = false WHERE pmid IN ({});".format(", ".join(pmids)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Falta archivo.")
+        exit()
+    sin_abstract(sys.argv[1])
