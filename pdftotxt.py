f44d997e7d6ca44cac90f82d9426b94b7e922fbd
Darién Julián Ramírez <ianshalaga@gmail.com>
2019-11-13 19:39:25 -0300

    pdftotext.py

--- pdftotxt.py
+++ pdftotxt.py
@@ -1,71 +1,26 @@
 # Compatibilidad entre Python 2 y 3
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from io import StringIO
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
+import sys
 import os
-import sys, getopt
-import re
-
-#converts pdf, returns its text content as a string
-#from https://www.binpress.com/tutorial/manipulating-pdfs-with-python/167
-def convert(fname, pages=None):
-    if not pages:
-        pagenums = set()
-    else:
-        pagenums = set(pages)
-
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-
-    infile = open(fname, 'rb')
-    for page in PDFPage.get_pages(infile, pagenums):
-        interpreter.process_page(page)
-    infile.close()
-    converter.close()
-    text = output.getvalue()
-    output.close
-    return text 
-   
-#converts all pdfs in directory pdfDir, saves all resulting txt files to txtdir
-def convertMultiple(pdfDir, txtDir):
-    if pdfDir == "": pdfDir = os.getcwd() + "\\" #if no pdfDir passed in 
-    for pdf in os.listdir(pdfDir): #iterate through pdfs in pdf directory
-        fileExtension = pdf.split(".")[-1]
-        if fileExtension == "pdf":
-            pdfFilename = pdfDir + pdf 
-            text = convert(pdfFilename) #get string of text content of pdf
-            # new_text = re.sub('[^a-zA-Z0-9\.]',' ',text) # Elimina caracteres especiales
-            textFilename = txtDir + pdf + ".txt"
-            textFile = open(textFilename, "w",encoding="utf8") #make text file
-            textFile.write(text) #write text to text file
-            # cat textFilename | tr -d '\r\n' > textFilename + '2.txt' 
-
-#i : info
-#p : pdfDir
-#t = txtDir
-def main(argv):
-    pdfDir = "E:/Descargas/Python/PFC_DGIdb_src/"
-    txtDir = "E:/Descargas/Python/PFC_DGIdb_src/"
-    try:
-        opts, args = getopt.getopt(argv,"ip:t:")
-    except getopt.GetoptError:
-        print("pdfToT.py -p <pdfdirectory> -t <textdirectory>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == "-i":
-            print("pdfToT.py -p <pdfdirectory> -t <textdirectory>")
-            sys.exit()
-        elif opt == "-p":
-            pdfDir = arg
-        elif opt == "-t":
-            txtDir = arg
-    convertMultiple(pdfDir, txtDir)
+import subprocess
     
+def pdftotext(ruta_pdf,ruta_txt):
+    xpdf_ruta = "E:/Descargas/Python/PFC_DGIdb_src/scraping/files/txt/pdftotext64.exe"
+    for archivo in sorted(os.listdir(ruta_pdf)):
+        if archivo.endswith(".pdf"):
+            entrada_ruta = os.path.join(ruta_pdf,archivo)
+            salida_nombre = archivo.replace(".pdf",".txt")
+            salida_ruta = os.path.join(ruta_txt,salida_nombre)
+            lista = [xpdf_ruta,"-enc","UTF-8","-nopgbrk","-nodiag",entrada_ruta,salida_ruta]
+            # subprocess.Popen(lista)
+            res = subprocess.run(lista)
+            if res.returncode != 0:
+                print("Error: " + archivo)
+
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if len(sys.argv) != 3:
+        print("Modo de uso: {} entrada salida".format(sys.argv[0])) # argv[0] es el nombre de la función
+        exit()
+
+    pdftotext(sys.argv[1],sys.argv[2])
