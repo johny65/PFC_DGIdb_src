746937c7fbac469b277cb5d27389909ccb27be7a
Juan Bertinetti <juanbertinetti@gmail.com>
2019-11-20 23:22:11 -0300

    Scrapeados alias de genes

--- scraping/aliases.py
+++ scraping/aliases.py
@@ -1,17 +1,20 @@
 """
-Web scraping de alias de genes de GeneCards.
+Web scraping de alias de genes.
 """
-import sys
 import utils
 import parallel
 import logging
+import argparse
+import csv
 from bs4 import BeautifulSoup
 
 BASE_GENECARDS_URL = "https://www.genecards.org/cgi-bin/carddisp.pl?gene="
+logger = logging.getLogger("aliases")
 
 
 class GeneCardsParser():
-    alias = []
+    """Web scraper de GeneCards."""
+    
     def fetch(self, gene):
         """Descarga el contenido de la página. NECESITA COOKIES PARA QUE ANDE."""
         url = BASE_GENECARDS_URL + gene
@@ -24,6 +27,7 @@ class GeneCardsParser():
         return data
 
     def __init__(self, gene, data=None):
+        self.alias = []
         if not data:
             data = self.fetch(gene)
         soup = BeautifulSoup(data, "html.parser")
@@ -42,9 +46,46 @@ class GeneCardsParser():
             print("No se encontraron alias para", gene)
 
 
+def process_line(elements):
+    """
+    'elements' es una lista con valores (un 'row' de un CSV). El gen es el primer elemento
+    ('elements[0]') y los demás son alias ya existentes. Devuelve una lista con el gen primero
+    y luego todos los alias recopilados ordenados alfabéticamente.
+    """
+    gen = elements[0]
+    print("Procesando gen {}...".format(gen), end=" ")
+    parser = GeneCardsParser(gen)
+    print("{} alias encontrados.".format(len(parser.alias)))
+    logger.info("Alias scrapeados: {}".format(parser.alias))
+    # unificar con los ya existentes, descartando numéricos:
+    all_alias = set(e.lower() for e in elements[1:] if not e.isnumeric()) #ya paso a lower
+    for alias in parser.alias:
+        all_alias.add(alias.lower())
+    res = [gen.lower()] + sorted(all_alias)
+    logger.info("Lista final: {}".format(res))
+    return res
+
+
 if __name__ == "__main__":
     # descomentar para activar logs para debug:
-    # logging.getLogger().setLevel(logging.DEBUG)
-    parser = GeneCardsParser("ABAT")
-    print("Cantidad de alias:", len(parser.alias))
-    print("Alias:", parser.alias)
+    # logger.setLevel(logging.INFO)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("entrada", help="Archivo de entrada", type=open)
+    parser.add_argument("salida", help="Archivo de salida", type=argparse.FileType("w"))
+    parser.add_argument("--test", help="Prueba que ande el scrap", action="store_true")
+    args = parser.parse_args()
+
+    if args.test:
+        gen = args.entrada.readline().split(",")[0]
+        gp = GeneCardsParser(gen)
+        print("Cantidad de alias para {}: {}".format(gen, len(gp.alias)))
+        print("Alias:", gp.alias)
+        exit()
+    
+    reader = csv.reader(args.entrada)
+    writer = csv.writer(args.salida)
+    for row in reader:
+        all_alias = process_line(row)
+        writer.writerow(all_alias)
+    
