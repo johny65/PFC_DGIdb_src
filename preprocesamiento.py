ddb4955c383ec94d4677ddc296351f45347a55f0
Juan Bertinetti <juanbertinetti@gmail.com>
2020-01-20 12:54:40 -0300

    Correcciones y mejoras en script de reemplazos

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -14,9 +14,9 @@ WRAPPER_FIN = "xxx"
 def reemplazo_inteligente(texto,cadena_a_remplazar,remplazar_con):
     return re.sub(r"\b{}\b".format(re.escape(cadena_a_remplazar)), remplazar_con, texto)
 
-def replace(original_text, search_for_replace, replace_with):
+def replace(original_text, search_for_replace, replace_with, pmid):
     """Wrapper a la función de reemplazo."""
-    logging.info("Reemplazando %s por %s", search_for_replace, replace_with)
+    logging.info("Pmid %s: Reemplazando %s por %s", pmid, search_for_replace, replace_with)
     # reemplazo básico:
     # return original_text.replace(search_for_replace, replace_with)
     # reemplazo inteligente con regex:
@@ -53,7 +53,7 @@ def full_reemplazar_bme(pmid, contents, alias_gen, alias_droga, etiquetas_genes,
             logging.error("Usando SIN REPETICIONES se encontró un alias de gen repetido!!: %s", og)
             logging.error("Diccionario: %s", str(genes))
         gen = list(genes)[0]
-        contents = replace(contents, og, WRAPPER_INI + gen + WRAPPER_FIN)
+        contents = replace(contents, og, WRAPPER_INI + gen + WRAPPER_FIN, pmid)
         if gen in genes_etiquetados:
             genes_etiquetados_encontrados.add(gen)
     
@@ -66,38 +66,38 @@ def full_reemplazar_bme(pmid, contents, alias_gen, alias_droga, etiquetas_genes,
             for g in genes: # si el alias se mapea a varios genes, tratar de elegir uno etiquetado
                 if g in genes_etiquetados:
                     gen = g
-                    logging.info("La ocurrencia %s tiene varios nombres reales de genes (%s), se eligió: %s por estar etiquetado",
-                                og, str(genes), gen)
+                    logging.info("Pmid %s: La ocurrencia %s tiene varios nombres reales de genes (%s), se eligió: %s por estar etiquetado",
+                                pmid, og, str(genes), gen)
                     if og == gen:
                         genes_etiquetados_encontrados.add(gen)
                     break
             if not gen:
                 # ninguno etiquetado, elijo cualquiera?
                 gen = list(genes)[random.randint(0, len(genes)-1)]
-                logging.warning("La ocurrencia %s tiene varios nombres reales de genes (%s), se eligió aleatoriamente: %s",
-                                og, str(genes), gen)
-            contents = replace(contents, og, WRAPPER_INI + gen + WRAPPER_FIN)
+                logging.warning("Pmid %s: La ocurrencia %s tiene varios nombres reales de genes (%s), se eligió aleatoriamente: %s",
+                                pmid, og, str(genes), gen)
+            contents = replace(contents, og, WRAPPER_INI + gen + WRAPPER_FIN, pmid)
             
             if set(genes_etiquetados) != genes_etiquetados_encontrados:
                 # analizo con embedding - con repetición
                 genes_etiquetados_no_encontrados = set(genes_etiquetados) - genes_etiquetados_encontrados
                 ocurrencias_repetidas_no_analizadas = set(ocurrencias_genes_todas[pmid]) - set(ocurrencias_genes_se_cr[pmid])
+                logging.info("Pmid %s: Analizando ahora gen con embedding - con repetición...", pmid)
                 for og in ocurrencias_repetidas_no_analizadas:
-                    logging.info("Analizando ahora gen con embedding - con repetición...")
                     genes = alias_gen[og]
                     gen = None
                     for g in genes: # si el alias se mapea a varios genes, tratar de elegir uno etiquetado
                         if g in genes_etiquetados_no_encontrados:
                             gen = g
-                            logging.info("La ocurrencia %s tiene varios nombres reales de genes (%s), se eligió: %s por estar etiquetado",
-                                        og, str(genes), gen)
+                            logging.info("Pmid %s: La ocurrencia %s tiene varios nombres reales de genes (%s), se eligió: %s por estar etiquetado",
+                                        pmid, og, str(genes), gen)
                             genes_etiquetados_encontrados.add(gen)
-                            contents = replace(contents, og, WRAPPER_INI + gen + WRAPPER_FIN)
+                            contents = replace(contents, og, WRAPPER_INI + gen + WRAPPER_FIN, pmid)
                             break
 
     genes_etiquetados_no_encontrados = set(genes_etiquetados) - genes_etiquetados_encontrados
     if genes_etiquetados_no_encontrados:
-        logging.info("De los genes etiquetados, no se encontró: %s", str(genes_etiquetados_no_encontrados))
+        logging.warning("Pmid %s: De los genes etiquetados, no se encontró: %s", pmid, str(genes_etiquetados_no_encontrados))
     
     
     
@@ -112,7 +112,7 @@ def full_reemplazar_bme(pmid, contents, alias_gen, alias_droga, etiquetas_genes,
             logging.error("Usando SIN REPETICIONES se encontró un alias de droga repetido!!: %s", og)
             logging.error("Diccionario: %s", str(drogas))
         droga = list(drogas)[0]
-        contents = replace(contents, og, WRAPPER_INI + droga + WRAPPER_FIN)
+        contents = replace(contents, og, WRAPPER_INI + droga + WRAPPER_FIN, pmid)
         if droga in drogas_etiquetadas:
             drogas_etiquetadas_encontradas.add(droga)
     
@@ -125,38 +125,38 @@ def full_reemplazar_bme(pmid, contents, alias_gen, alias_droga, etiquetas_genes,
             for d in drogas: # si el alias se mapea a varios drogas, tratar de elegir uno etiquetado
                 if d in drogas_etiquetadas:
                     droga = d
-                    logging.info("La ocurrencia %s tiene varios nombres reales de drogas (%s), se eligió: %s por estar etiquetado",
-                                og, str(drogas), droga)
+                    logging.info("Pmid %s: La ocurrencia %s tiene varios nombres reales de drogas (%s), se eligió: %s por estar etiquetado",
+                                 pmid, og, str(drogas), droga)
                     if og == droga:
                         drogas_etiquetadas_encontradas.add(droga)
                     break
             if not droga:
                 # ninguno etiquetado, elijo cualquiera?
                 droga = list(drogas)[random.randint(0, len(drogas)-1)]
-                logging.warning("La ocurrencia %s tiene varios nombres reales de drogas (%s), se eligió aleatoriamente: %s",
-                                og, str(drogas), droga)
-            contents = replace(contents, og, WRAPPER_INI + droga + WRAPPER_FIN)
+                logging.warning("Pmid %s: La ocurrencia %s tiene varios nombres reales de drogas (%s), se eligió aleatoriamente: %s",
+                                pmid, og, str(drogas), droga)
+            contents = replace(contents, og, WRAPPER_INI + droga + WRAPPER_FIN, pmid)
             
             if set(drogas_etiquetadas) != drogas_etiquetadas_encontradas:
                 # analizo con embedding - con repetición
                 drogas_etiquetadas_no_encontradas = set(drogas_etiquetadas) - drogas_etiquetadas_encontradas
                 ocurrencias_repetidas_no_analizadas = set(ocurrencias_drogas_todas[pmid]) - set(ocurrencias_drogas_se_cr[pmid])
+                logging.info("Pmid %s: Analizando droga ahora con embedding - con repetición...", pmid)
                 for og in ocurrencias_repetidas_no_analizadas:
-                    logging.info("Analizando droga ahora con embedding - con repetición...")
                     drogas = alias_droga[og]
                     droga = None
                     for g in drogas: # si el alias se mapea a varios drogas, tratar de elegir uno etiquetado
                         if g in drogas_etiquetadas_no_encontradas:
                             droga = g
-                            logging.info("La ocurrencia %s tiene varios nombres reales de drogas (%s), se eligió: %s por estar etiquetado",
-                                        og, str(drogas), droga)
+                            logging.info("Pmid %s: La ocurrencia %s tiene varios nombres reales de drogas (%s), se eligió: %s por estar etiquetado",
+                                         pmid, og, str(drogas), droga)
                             drogas_etiquetadas_encontradas.add(droga)
-                            contents = replace(contents, og, WRAPPER_INI + droga + WRAPPER_FIN)
+                            contents = replace(contents, og, WRAPPER_INI + droga + WRAPPER_FIN, pmid)
                             break
 
     drogas_etiquetadas_no_encontradas = set(drogas_etiquetadas) - drogas_etiquetadas_encontradas
     if drogas_etiquetadas_no_encontradas:
-        logging.info("De las drogas etiquetadas, no se encontró: %s", str(drogas_etiquetadas_no_encontradas))
+        logging.warning("Pmid %s: De las drogas etiquetadas, no se encontró: %s", pmid, str(drogas_etiquetadas_no_encontradas))
     
     if todisk:
         with open(output_dir / (pmid + ".txt"), "w") as f:
@@ -166,7 +166,7 @@ def full_reemplazar_bme(pmid, contents, alias_gen, alias_droga, etiquetas_genes,
 
 
 if __name__ == "__main__":
-    logging.basicConfig(logging.INFO)
+    logging.basicConfig(level=logging.INFO)
     # logging.getLogger("datos_preprocesamiento").setLevel(logging.INFO)
 
     lista_pmid = "PFC_DGIdb/pmids_etiquetas_completas.csv"
@@ -188,7 +188,7 @@ if __name__ == "__main__":
     if not output_dir.exists():
         output_dir.mkdir()
 
-    dir_pfc_dgidb = pathlib.Path(args.dir_pfc_dgidb)
+    dir_pfc_dgidb = pathlib.Path(dir_pfc_dgidb)
     alias_gen = cargar_aliases_dict(dir_pfc_dgidb / "alias_gen.csv")
     alias_droga = cargar_aliases_dict(dir_pfc_dgidb / "alias_droga.csv")
 
